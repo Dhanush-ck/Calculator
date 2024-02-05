ae2ae9765f55f8816c30e11ed41050a6f2e658f4
Dhanush CK <152354985+Dhanush-ck@users.noreply.github.com>
2024-02-05 17:38:47 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,31 @@
+function toggleButton(buttonId) {
+    const button = document.getElementById(buttonId);
+    // button.classList.toggle('active');
+    if (button){
+    button.addEventListener('click', function (event) {
+        const isButtonActive = button.classList.contains('active');
+        const nm = button.getAttribute('name');
+        button.classList.toggle('active');
+        localStorage.setItem('isButtonActive', String(!isButtonActive));
+        localStorage.setItem('name',nm);
+        
+        });
+    }
+    }
+    toggleButton('togglebutton');
+    toggleButton('togglebutton1');
+// function toggleButton(buttonId) {
+//         const button = document.getElementById(buttonId);
+//         button.classList.toggle('active');
+//         const isButtonActive = button.classList.contains('active');
+//         const name = button.getAttribute('name');
+//         localStorage.setItem('isButtonActive', isButtonActive);
+//         localStorage.setItem('name', name);
+//     }
+
+    // document.getElementsByClassName('switch').addEventListener('click',buttonId);
+
+
+function homePage() {
+    window.location.href = 'D:/Codes/Prgrms/HTML/Calculator/index.html';
+}
