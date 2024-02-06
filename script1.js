9a76d60a9441838cb541ec8d3ada1990483393f3
Dhanush CK <152354985+Dhanush-ck@users.noreply.github.com>
2024-02-06 20:35:49 +0530

    Clean up structure of the code

--- script1.js
+++ script1.js
@@ -1,6 +1,5 @@
 function toggleButton(buttonId) {
     const button = document.getElementById(buttonId);
-    // button.classList.toggle('active');
     if (button){
     button.addEventListener('click', function (event) {
         const isButtonActive = button.classList.contains('active');
@@ -15,16 +14,6 @@ function toggleButton(buttonId) {
     toggleButton('toggleButton');
     toggleButton('toggleButton1');
     toggleButton('toggleButton2');
-// function toggleButton(buttonId) {
-//         const button = document.getElementById(buttonId);
-//         button.classList.toggle('active');
-//         const isButtonActive = button.classList.contains('active');
-//         const name = button.getAttribute('name');
-//         localStorage.setItem('isButtonActive', isButtonActive);
-//         localStorage.setItem('name', name);
-//     }
-
-    // document.getElementsByClassName('switch').addEventListener('click',buttonId);
 
 
 function homePage() {
